b91d97b0cbb630f19075ef43acbc46d1b991f944
Chloe Haynes <chloehaynes5252@gmail.com>
2025-06-20 22:32:29 -0600

    added bio

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -4,8 +4,10 @@ export const Banner = () => {
         <section className="banner">
             <div className="banner-content">
                 <h1>Hi! I'm Chloe</h1>
-                <p>I am a Computing Science student at the University of Alberta. 
-                    I love software dev, data science, and anything creative.</p>
+                <p>I am a 4th year Computing Science Student at the University of Alberta. 
+                    I love software dev, data science, and anything creative. 
+                    I am currently seeking internship opportunities where I can contribute, learn,
+                    and grow as a developer.</p>
                 <p>Check out my projects below!</p>
                  <p className="hint">psst.. press space bar to change colors</p>
                 {/* <a href="/Projects" className="btn">View Projects</a> */}
